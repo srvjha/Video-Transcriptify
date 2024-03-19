448a142a3b7a69a98b6705c419b73d4798427b16
Saurav Jha <srvtj629@gmail.com>
2024-03-19 13:05:02 +0530

    Created Utils

---
+++ backend/src/app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+const app =express();
+app.use(cors({    
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+// app.use() is used to mount the CORS middleware globally to the Express application. It specifies the CORS configuration options, including the allowed origin (retrieved from the environment variable CORS_ORIGIN) and whether credentials (such as cookies) should be allowed in cross-origin requests.
+
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+export{app}
--- backend/src/main.js
+++ backend/src/main.js
@@ -1,8 +1,20 @@
 import express from 'express'
 import connectDB from './db/index.js'
 import dotenv from 'dotenv'
+import { app } from './app.js'
 
 dotenv.config({
     path:'./.env'
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`SERVER is running at PORT:- ${process.env.PORT}`)
+    })
+    app.on("error",()=>{
+        console.log("ERROR OCCURRED WHILE CONNECTION ",error)
+    })
+})
+.catch((error)=>{
+    console.log("MONGODB Connection Error. ",error)
+})
