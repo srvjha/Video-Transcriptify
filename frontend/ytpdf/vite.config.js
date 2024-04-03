f7320b69f2e53990e8ac26636cca688924453cb4
Saurav Jha <srvjt629@gmail.com>
2024-04-03 19:39:36 +0530

    bug solved

--- frontend/ytpdf/vite.config.js
+++ frontend/ytpdf/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':"https://video-transcriptify-app.onrender.com"
+      '/api':"https://video-transcriptify-app.onrender.com/api"
     }
   },
   plugins: [react()],
