884936d4b50baf5d4f98b99e5901c121d8a4ffb1
Saurav Jha <srvjt629@gmail.com>
2024-04-06 20:08:46 +0530

    vercel json added

--- frontend/ytpdf/vite.config.js
+++ frontend/ytpdf/vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    proxy:{
-      target: 'https://video-transcriptify-app.onrender.com',
-      changeOrigin: true,
-      rewrite: path => path.replace(/^\/api/, '')
-    }
-  },
+  // server:{
+  //   proxy:{
+  //     target: 'https://video-transcriptify-app.onrender.com',
+  //     changeOrigin: true,
+  //     rewrite: path => path.replace(/^\/api/, '')
+  //   }
+  // },
   plugins: [react()],
 
 })
