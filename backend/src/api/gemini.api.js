f80236484a1527361b2d801e27fe75a8044c6fba
Saurav Jha <jhasaurav0209001@gmail.com>
2024-05-19 03:31:06 +0530

    Integrated Gemini API

---
+++ backend/src/api/gemini.api.js
@@ -0,0 +1,25 @@
+import { GoogleGenerativeAI } from"@google/generative-ai";
+import express from 'express'
+
+const app = express()
+app.use(express.json());
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+
+
+// For text-only input, use the gemini-pro model
+const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+
+export async function  enhanceWithGemini(transcript) {
+    const prompt = `Make notes of these raw text ${transcript} and please dont use any * as it is stored 
+    in pdf so there this syntax sugar wont work just plain normal text`
+  
+    const result = await model.generateContent(prompt);
+    const response = result.response;
+    const text = response.text();
+    return text
+  }
+  
+  
