c1e86a27cc631386a83adb8b6867309da87c3f43
Saurav Jha <srvtj629@gmail.com>
2024-05-28 14:16:08 +0530

    Improved the prompt

--- backend/src/api/gemini.api.js
+++ backend/src/api/gemini.api.js
@@ -23,15 +23,17 @@ export async function  enhanceWithGemini(transcript) {
      
      **Instructions:**
      
-     1. Please create concise notes summarizing the key points and highlights from the transcript.
-     2. **In case of encountering inappropriate content, prioritize summarizing the key points ethically and omit any offensive language or themes.**
-     3. Ensure that the generated notes are clear, coherent, and suitable for all audiences.
-     4. Avoid using any special characters or syntax such as "*" as the notes will be stored in a PDF format.
-     5. If the transcript includes any abuses for summarization then only, state this: "The provided transcript cannot be generated due to one of the following reasons:-
+     1. Please create concise notes summarizing the key points and highlights from the transcript.\
+     2. Please emoji wherever required to express the emotion.
+     3. **In case of encountering inappropriate content, prioritize summarizing the key points ethically and omit any offensive language or themes.**
+     4. Ensure that the generated notes are clear, coherent, and suitable for all audiences.
+     5. Avoid using any special characters or syntax such as "*" as the notes will be stored in a PDF format.
+     6. If the transcript includes any abuses for summarization then only, state this: "The provided transcript cannot be generated due to one of the following reasons:-
      - It would have contained extensive inappropriate content that cannot be ethically summarized 
      - It was not something considered for education. 
-     Please consider providing a different transcript or editing the current one,
-     but keep in mind that you have to provide notes so just remove inappropriate and provide clean notes"
+     Please consider providing a better URL,
+     BETTER LUCK NEXT TIME"
+
      
      Thank you for your assistance!
 
